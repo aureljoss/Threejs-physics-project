5f6749970cd0c90d77e2ddc775b6ccff1ed001dd
Aurelie <josserand.n.aurelie@gmail.com>
2024-10-19 10:52:43 -0400

    Adds Cannon library, adds physics to sphere

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
+import CANNON from 'cannon'
+
 
 /**
  * Debug
@@ -31,21 +33,62 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/nz.png'
 ])
 
+
+/**
+ * Physics
+ */
+//World
+const world= new CANNON.World()
+world.gravity.set(0,-9.82, 0)
+
+//Material
+const defaultMaterial= new CANNON.Material('default')
+
+const defaultContactMaterial= new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.9
+    }
+)
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial=defaultContactMaterial
+
+
+//Sphere
+// const sphereShape= new CANNON.Sphere(0.5)
+// const sphereBody= new CANNON.Body({
+//     mass:1,
+//     position: new CANNON.Vec3(0,3,0),
+//     shape: sphereShape, 
+// })
+// sphereBody.applyLocalForce(new CANNON.Vec3(150,0,0), new CANNON.Vec3(0,0,0))
+// world.addBody(sphereBody)
+
+//Floor
+const floorShape= new CANNON.Plane()
+const floorBody= new CANNON.Body()
+floorBody.mass=0
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1,0,0), Math.PI*0.5)
+world.addBody(floorBody)
+
 /**
  * Test sphere
  */
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    new THREE.MeshStandardMaterial({
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture,
-        envMapIntensity: 0.5
-    })
-)
-sphere.castShadow = true
-sphere.position.y = 0.5
-scene.add(sphere)
+// const sphere = new THREE.Mesh(
+//     new THREE.SphereGeometry(0.5, 32, 32),
+//     new THREE.MeshStandardMaterial({
+//         metalness: 0.3,
+//         roughness: 0.4,
+//         envMap: environmentMapTexture,
+//         envMapIntensity: 0.5
+//     })
+// )
+// sphere.castShadow = true
+// sphere.position.y = 0.5
+// scene.add(sphere)
 
 /**
  * Floor
@@ -131,10 +174,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let oldElapsedTime=0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime= elapsedTime-oldElapsedTime
+    oldElapsedTime= elapsedTime
+
+    //Update physics world
+    // sphereBody.applyForce(new CANNON.Vec3(-0.5,0,0), sphereBody.position)
+    world.step(1/60, deltaTime, 3)
+
+    // sphere.position.copy(sphereBody.position)
 
     // Update controls
     controls.update()
